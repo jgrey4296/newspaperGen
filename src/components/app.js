bccc57dbbd6b6f30f6a77e40ba7fd215359acfcc
John Grey <jgrey@ucsc.edu>
2017-03-22 13:50:38 +0000

    added a router to the top level of the app

--- src/components/app.js
+++ src/components/app.js
@@ -1,22 +1,32 @@
 import { h, Component } from 'preact';
+import Router from 'preact-router';
 import Header from './header';
-import Home from './home';
-import Help from './help';
-import Footer from './footer';
-import { Shell } from 'JGShell';
+import FrontPage from './frontpage';
+import Obits from './obits';
+import Ads from './ads';
+import Sports from './sports';
 
 export default class App extends Component {
     constructor(){
         super();
-    }
+    };
 
+    handleRoute = (e) => {
+        this.currentUrl = e.url;
+    };
+    
     render() {
         return (
-			    <div id="app">
-			    <Header path={this.state.path} />
-                {body}
-                <Footer callback={(text) => { this.parseCallback(text); }} />
-			    </div>
+            <div id="app">
+			<Header />
+            <Router onChange={this.handleRoute}>
+            <FrontPage path="/" data={[]} />
+            <Obits path="/obits/" data={[]} />
+            <Ads path="/ads/" data={[]} />
+            <Sports path="/sports/" data={[]} />            
+            </Router>
+            </div>
         );
     }
+
 }
