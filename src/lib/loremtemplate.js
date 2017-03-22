7e46655f41f714eee2fecfa9a9e9400f8d47ee38
John Grey <jgrey@ucsc.edu>
2017-03-22 15:05:23 +0000

    added a default lorem ipsum setup

---
+++ src/lib/loremtemplate.js
@@ -0,0 +1,12 @@
+let loremtemplate = {
+    count: 2,
+    units: 'paragraphs',
+    sentenceLowerBound: 5,
+    sentenceUpperBound: 15,
+    paragraphLowerBound: 3,
+    paragraphUpperBound: 7,
+    format: 'plain',
+    suffix: null
+};
+
+export default loremtemplate;
