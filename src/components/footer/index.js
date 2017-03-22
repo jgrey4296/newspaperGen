d282e485802e08fe5dab6801e8cb52ed91c7e43e
John Grey <jgrey@ucsc.edu>
2017-03-22 15:05:14 +0000

    added stubb directories and components

---
+++ src/components/footer/index.js
@@ -0,0 +1,29 @@
+import { h, Component } from 'preact';
+import React from 'react'; //aliased, for proptypes
+import style from './style';
+
+export default class Footer extends Component {
+    //snippet::proptypes
+
+    constructor(props){
+        super(props);
+        //modify with this.setState
+        this.state = {
+            
+        };
+
+        //if necessary, bind methods
+        //this.aFunc = this.aFunc.bind(this)
+    }
+
+    //Don't forget to destructure:
+    render(props, state){
+        return (
+                <div>
+
+            </div>
+        );
+    }
+
+
+}
---
+++ src/components/subarticle/index.js
@@ -0,0 +1,47 @@
+import { h, Component } from 'preact';
+import React from 'react'; //aliased, for proptypes
+import style from './style';
+import _ from 'lodash';
+import loremipsum from 'lorem-ipsum';
+import loremtemplate from 'loremtemplate';
+
+export default class SubArticle extends Component {
+    //snippet::proptypes
+
+    constructor(props){
+        super(props);
+        //modify with this.setState
+        this.state = {
+            
+        };
+
+        //if necessary, bind methods
+        //this.aFunc = this.aFunc.bind(this)
+    }
+
+    names = [
+        "Barbara Thropewood",
+        "Kinglsey Armlet",
+        "Bob Griddle",
+        "Elanor Fidget"
+    ];
+    
+    //Don't forget to destructure:
+    render({ title }, state){
+        let name = _.sample(this.names);
+        return (
+            <span>
+            <div class={style.wrapper}>
+            <h2>{title}</h2>
+            <h4> by {name}</h4>
+            </div>
+            <div class={style.text}>
+            {loremipsum(loremtemplate)}
+            <div class={style.hline_light} />
+            </div>
+            </span>
+        );
+    }
+
+
+}
